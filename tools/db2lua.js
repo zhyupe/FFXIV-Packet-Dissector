813c2114efeb59acc4569904390680b9e3d0ed35
Innpaul Zhang <zyp108421@gmail.com>
2020-02-06 23:44:09 +0800

    feat(db): update to 5.1

--- tools/db2lua.js
+++ tools/db2lua.js
@@ -2,7 +2,7 @@ const fs = require('fs')
 const path = require('path')
 const readCsv = require('./read-csv')
 
-const dbRoot = '../../library/5.05/'
+const dbRoot = '../../library/5.1/'
 const output = '../src/'
 const langs = ['chs', 'en', 'ja', 'de', 'fr']
 
