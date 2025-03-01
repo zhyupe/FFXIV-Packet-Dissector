7c35dad514dcc5dd6609ce99234abb0819b7d1a3
zhyupe <zyp108421@gmail.com>
2025-03-01 11:06:47 +0800

    chore: update db

--- tools/db2lua.js
+++ tools/db2lua.js
@@ -2,7 +2,7 @@ const fs = require('fs')
 const path = require('path')
 const readCsv = require('./read-csv')
 
-const dbRoot = '../../library/6.01/'
+const dbRoot = '../../library/7.1/'
 const output = '../src/'
 const langs = ['chs', 'en', 'ja', 'de', 'fr']
 
